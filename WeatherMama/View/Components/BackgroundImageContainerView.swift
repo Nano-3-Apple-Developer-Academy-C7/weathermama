08f14948549f244f5ed89eef11e2d814c7ef57a6
LuckyBastrd <Rafi.Khairunizham@gmail.com>
2024-07-16 15:07:47 +0700

    WM-16 <Frontend and UI Components>

---
+++ WeatherMama/View/Components/BackgroundImageContainerView.swift
@@ -0,0 +1,46 @@
+//
+//  BackgroundImageContainerView.swift
+//  WeatherMama
+//
+//  Created by Lucky on 13/07/24.
+//
+
+import UIKit
+
+class BackgroundImageContainerView: UIView {
+    private lazy var backgroundImage: UIImageView = {
+        let backgroundImage = UIImage(named: "bright")
+        let backgroundImageView = UIImageView(image: backgroundImage)
+        
+        backgroundImageView.contentMode = .scaleToFill
+        self.addSubview(backgroundImageView)
+        self.sendSubviewToBack(backgroundImageView)
+        
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return backgroundImageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupBackgroundImage()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension BackgroundImageContainerView {
+    private func setupBackgroundImage() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(backgroundImage)
+        
+        NSLayoutConstraint.activate([
+            backgroundImage.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            backgroundImage.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+        ])
+    }
+}
---
+++ WeatherMama/View/Components/TemperatureView.swift
@@ -0,0 +1,89 @@
+//
+//  TemperatureView.swift
+//  WeatherMama
+//
+//  Created by Lucky on 16/07/24.
+//
+
+import UIKit
+
+class TemperatureView: UIView {
+    
+    // TEMPERATURE DETAIL
+    private lazy var temperatureStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 12
+        
+        return stackView
+    }()
+    
+    private lazy var temperatureTitleLabel: UIView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 2
+        
+        let symbolImageView = UIImageView()
+        symbolImageView.image = UIImage(systemName: "thermometer.medium")
+        symbolImageView.tintColor = .secondaryLabel
+        symbolImageView.contentMode = .scaleAspectFit  // Ensure symbol scales correctly
+        symbolImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let titleLabel = UILabel()
+        titleLabel.text = "TEMPERATURE"
+        titleLabel.font = UIFont(name: "SFProRounded-Regular", size: 12)
+        titleLabel.textColor = .secondaryLabel
+        titleLabel.textAlignment = .center
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView.addArrangedSubview(symbolImageView)
+        stackView.addArrangedSubview(titleLabel)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+    }()
+    
+    private lazy var temperatureRateLabel: UILabel = {
+        let temperatureRateLabel = UILabel()
+        temperatureRateLabel.text = "32°"
+        temperatureRateLabel.font = UIFont(name: "SFProRounded-Regular", size: 20)
+        temperatureRateLabel.textColor = .black
+        temperatureRateLabel.textAlignment = .center
+        temperatureRateLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        return temperatureRateLabel
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupTemperature()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension TemperatureView {
+
+    private func setupTemperature() {
+        self.translatesAutoresizingMaskIntoConstraints = false 
+        
+        self.addSubview(temperatureStackView)
+        
+        NSLayoutConstraint.activate([
+            temperatureStackView.widthAnchor.constraint(equalToConstant: 100),
+            temperatureStackView.heightAnchor.constraint(equalToConstant: 71),
+        ])
+        
+        temperatureStackView.addArrangedSubview(temperatureTitleLabel)
+        
+        temperatureStackView.addArrangedSubview(temperatureRateLabel)
+    }
+}
+
